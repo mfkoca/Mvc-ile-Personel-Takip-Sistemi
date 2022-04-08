2e999774a0066b8b7fcb87cff5709bc65cc7e8f3
Mehmet Fatih Koca <mehmedfatihkoca@gmail.com>
2022-04-09 02:12:44 +0300

    mvc

---
+++ PersonelMvc/PersonelMvc/Controllers/DepartmanController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonelMvc.Models.EntityFramework;
+
+namespace PersonelMvc.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        MvcEntities1 db = new MvcEntities1();
+        public ActionResult Index()
+        {
+            var model = db.Departman.ToList();
+            return View(model);
+        }
+        [HttpGet] //veriyi okuma öz nitelik
+        public ActionResult Yeni()
+        {
+            return View("DepartmanFrom");
+        }
+        [HttpPost]
+        public ActionResult Kaydet(Departman departman)
+        {
+            if (departman.id==0) // id 0 ise yeni kayıt
+            {
+                db.Departman.Add(departman);
+            }
+            else // id si olan kaydı güncelle 
+            {
+                var guncellenecekdepartman = db.Departman.Find(departman.id);
+                if (guncellenecekdepartman==null)
+                {
+                    return HttpNotFound();
+                }
+                guncellenecekdepartman.Ad = departman.Ad;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index","Departman");
+        }
+        public ActionResult Guncelle(int id)
+        {
+            var model = db.Departman.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View("DepartmanFrom", model);
+        }
+        public ActionResult Sil(int id)
+        {
+            var sildepartman = db.Departman.Find(id);
+            if (sildepartman==null)
+            {
+                return HttpNotFound();
+            }
+            db.Departman.Remove(sildepartman);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PersonelMvc/PersonelMvc/Controllers/PersonelController.cs
@@ -0,0 +1,65 @@
+﻿using PersonelMvc.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonelMvc.Controllers;
+using System.Data.Entity;
+using PersonelMvc.ViewModels;
+
+namespace PersonelMvc.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        MvcEntities1 db = new MvcEntities1();
+        public ActionResult Index()
+        {
+            var model = db.Personel.Include(x=>x.Departman).ToList();
+            return View(model);
+        }
+        public ActionResult Yeni()
+        {
+            var model = new PersonelFormViewModel()
+            {
+                Departmanlar = db.Departman.ToList()
+            };
+            return View("PersonelForm",model);
+        }
+        public ActionResult Kaydet(Personel personel)
+        {
+            if (personel.id == 0) // id 0 ise yeni kayıt
+            {
+                db.Personel.Add(personel);
+            }
+            else // id si olan kaydı güncelle 
+            {
+                // bu satırda id bilgisini almadan o an ki kayıdın güncellemesini sağluyoruz.
+                db.Entry(personel).State=System.Data.Entity.EntityState.Modified;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");  // index sayfasına yönlerdiriyor.
+        }
+        public ActionResult Guncelle(int id)
+        {
+            var model = new PersonelFormViewModel()
+            {
+                Departmanlar =db.Departman.ToList(),
+                Personel=db.Personel.Find(id)
+            };
+            return View("PersonelForm", model);
+        }
+        public ActionResult Sil(int id)
+        {
+            var silpersonel = db.Personel.Find(id);
+            if (silpersonel == null)
+            {
+                return HttpNotFound();
+            }
+            db.Personel.Remove(silpersonel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
